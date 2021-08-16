786544d316c95cbb67116b59479b9d75139f5667
AlexandreC93 <alexandrecadinot93@gmail.com>
2021-08-16 17:24:35 +0200

    ytes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import './styles/global.css'
+import Box from './components/Box'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const tempMin = -20
+const tempMax = 40
+const heartMin = 80
+const heartMax = 180
+const stepsMin = 0
+const stepsMax = 50000
+
+
+
+export class App extends React.Component {
+
+  render() {
+
+    return (
+
+
+
+      <div className="container-fluid">
+        <div className="row">
+          <p>
+            Water : 
+            Heart : {heartMin}
+            Temperature : {tempMin}
+            Steps : {stepsMin}
+          </p>
+
+        
+          <Box icon="local_drink" color="#3A85FF" value={1.5} unit="L" />
+          <Box icon="favorite" color="red" value={120} unit="bpm" />
+
+          <Box color="yellow" icon="wb_sunny" value={-10} unit="°C" />
+
+          <Box color="black" icon="directions_walk" value="Steps" unit="0" />
+        </div>
+
+      </div>
+
+
+    );
+  }
 }
 
 export default App;
+
